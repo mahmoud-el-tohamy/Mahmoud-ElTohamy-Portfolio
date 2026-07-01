6466ff8539abd7149d3ea4373b6bbe26de9513fd
Mahmoud El-Tohamy <mahmoud.eltohamyy@gmail.com>
2026-07-02 00:09:31 +0300

    feat: integrate Vercel Analytics for visitor tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
 import "../styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import SmoothScroll from "../components/SmoothScroll";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = ({ Component, pageProps }) => {
   return (
     <ThemeProvider attribute="class">
       <SmoothScroll>
         <Component {...pageProps} />
+        <Analytics />
       </SmoothScroll>
     </ThemeProvider>
   );
